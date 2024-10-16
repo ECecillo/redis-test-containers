b6e4b47606f6a29cc453bfbc2bf46b5d90771f1e
ECecillo <enzo@batch.com>
2024-10-16 14:48:07 +0200

    chore: setup a composer and ping the database in the main file

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"context"
+	"log"
+	"redis-connection/example/repository"
+)
+
+// Just a simple setup to test if we can correctly ping, pong the redis server.
 func main() {
-	// ExampleClient()
+	redisClient, err := repository.NewRedisClient("localhost:6379", "test")
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = redisClient.Ping(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
 }
