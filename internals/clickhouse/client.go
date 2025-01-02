4fe3c1acb440d2254cac1ec63de59410f7fc8751
ECecillo <enzo@batch.com>
2025-01-02 17:02:02 +0100

    chore(client): define config structure for connection parm

--- internals/clickhouse/client.go
+++ internals/clickhouse/client.go
@@ -3,11 +3,32 @@ package clickhouse
 import (
 	"context"
 	"redis-connection/example/pkg/repository"
+
+	"go.uber.org/zap"
 )
 
-type ClickHouseClient struct{}
+type ClickHouseClient struct {
+	logger *zap.Logger
+}
 
 var _ repository.Repository = &ClickHouseClient{}
+
+type Config struct {
+	Host string
+	Port int
+
+	DatabaseName string
+	User         string
+	Password     string
+}
+
+func NewClickHouseClient(logger *zap.Logger, conf Config) *ClickHouseClient {
+
+	return &ClickHouseClient{
+		logger: logger.Named("clickhouse-client"),
+		//TODO: add connection
+	}
+}
 
 // DeleteCounter implements repository.Repository.
 func (c *ClickHouseClient) DeleteCounter(ctx context.Context, counterKey string) (ok bool, err error) {
