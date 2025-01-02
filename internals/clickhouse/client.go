f84573bf6e26ed02901fab8ef7d01ff4c43136c1
ECecillo <enzo@batch.com>
2025-01-02 16:56:03 +0100

    chore(clickhouse): setup with panic for unimplemented function

--- internals/clickhouse/client.go
+++ internals/clickhouse/client.go
@@ -1,1 +1,25 @@
 package clickhouse
+
+import (
+	"context"
+	"redis-connection/example/pkg/repository"
+)
+
+type ClickHouseClient struct{}
+
+var _ repository.Repository = &ClickHouseClient{}
+
+// DeleteCounter implements repository.Repository.
+func (c *ClickHouseClient) DeleteCounter(ctx context.Context, counterKey string) (ok bool, err error) {
+	panic("unimplemented")
+}
+
+// GetCounterValue implements repository.Repository.
+func (c *ClickHouseClient) GetCounterValue(ctx context.Context, counterKey string) (counterValue int, err error) {
+	panic("unimplemented")
+}
+
+// UpsertCounterValue implements repository.Repository.
+func (c *ClickHouseClient) UpsertCounterValue(ctx context.Context, counterKey string) (newCounterValue int, err error) {
+	panic("unimplemented")
+}
