9585904733ec2cd2c26ed69e4ea8935b23d0e230
ECecillo <enzo@batch.com>
2025-01-03 08:55:09 +0100

    wip(clickhouse): implement repository interface operation

--- internals/clickhouse/client.go
+++ internals/clickhouse/client.go
@@ -1,3 +1,10 @@
+// clickhouse package define a client to send request to a ClickHouse server.
+// It implements repository.Repository interface in order to have business operation
+// for our counter component.
+//
+// NOTE: ClickHouse does not fit as a counter store but the main purpose of
+// this repository is to explore different kind of SGBD and setup testcontainers
+// to run integration tests.
 package clickhouse
 
 import (
@@ -17,6 +24,8 @@ type ClickHouseClient struct {
 
 var _ repository.Repository = &ClickHouseClient{}
 
+// NewClickHouseClient is a constructor for a ClickHouseClient, it will setup for us
+// the connection between our app and the specified database server.
 func NewClickHouseClient(logger *zap.Logger, conf Config) (*ClickHouseClient, error) {
 
 	conn, err := connect(conf)
@@ -32,19 +41,87 @@ func NewClickHouseClient(logger *zap.Logger, conf Config) (*ClickHouseClient, er
 
 // DeleteCounter implements repository.Repository.
 func (c *ClickHouseClient) DeleteCounter(ctx context.Context, counterKey string) (ok bool, err error) {
-	panic("unimplemented")
+
+	logger := c.logger.Named("delete").With(
+		zap.String("counter_key", counterKey),
+	)
+
+	logger.Info("start to delete counter value")
+
+	if err = c.conn.Exec(ctx, "DELETE FROM counter WHERE Id = ?", counterKey); err != nil {
+		logger.Warn("unable to delete counter",
+			zap.Error(err),
+		)
+		return false, fmt.Errorf("unable to delete counter, err: %w", err)
+	}
+
+	logger.Debug("successfully removed counter")
+
+	return true, nil
 }
 
 // GetCounterValue implements repository.Repository.
 func (c *ClickHouseClient) GetCounterValue(ctx context.Context, counterKey string) (counterValue int, err error) {
-	panic("unimplemented")
+
+	var ckCounter Counter
+
+	logger := c.logger.Named("get").With(
+		zap.String("counter_key", counterKey),
+	)
+
+	if err = c.conn.Select(ctx, &ckCounter, "SELECT Id, CounterValue FROM counter WHERE Id = ?", counterKey); err != nil {
+		logger.Warn("unable to retrieve counter value",
+			zap.Error(err),
+		)
+		return 0, fmt.Errorf("unable to retrieve counter value, err: %w", err)
+	}
+
+	return ckCounter.CounterValue, nil
+}
+
+type Counter struct {
+	Id           string
+	CounterValue int
 }
 
 // UpsertCounterValue implements repository.Repository.
 func (c *ClickHouseClient) UpsertCounterValue(ctx context.Context, counterKey string) (newCounterValue int, err error) {
-	panic("unimplemented")
+	// Since upsert command does not exist in clickhouse we are going to make a SELECT followed by INSERT with new value.
+	logger := c.logger.Named("upsert").With(
+		zap.String("counter_key", counterKey),
+	)
+
+	actualValue, err := c.GetCounterValue(ctx, counterKey)
+	if err != nil {
+		logger.Warn("unable to get counter value",
+			zap.Error(err),
+		)
+		return 0, fmt.Errorf("unable to get counter value, err: %w", err)
+	}
+
+	newCounterValue = actualValue + 1
+
+	logger = logger.With(
+		zap.Int("new_counter_value", newCounterValue),
+		zap.Int("previous_counter_value", actualValue),
+	)
+
+	logger.Debug("calculated new counter value")
+
+	err = c.conn.Exec(ctx, "INSERT INTO counter VALUES (?, ?)", counterKey, newCounterValue)
+	if err != nil {
+		logger.Warn("unable to insert new counter value",
+			zap.Error(err),
+		)
+		return 0, fmt.Errorf("unable to insert new counter value, err: %w", err)
+	}
+
+	logger.Info("new counter value inserted")
+
+	return newCounterValue, nil
 }
 
+// Ping just check if we can
 func (c ClickHouseClient) Ping(ctx context.Context) error {
 	logger := c.logger.Named("ping")
 
