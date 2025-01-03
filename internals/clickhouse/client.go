7adfe6e4c927a4fa64ea6f9b3f9b9ca87e07142a
ECecillo <enzo@batch.com>
2025-01-03 08:59:13 +0100

    doc: redis client package and improve clickhouse description

--- internals/clickhouse/client.go
+++ internals/clickhouse/client.go
@@ -1,10 +1,10 @@
 // clickhouse package define a client to send request to a ClickHouse server.
-// It implements repository.Repository interface in order to have business operation
-// for our counter component.
+// It implements repository.Repository interface in order to run business
+// logic related to our counter component.
 //
-// NOTE: ClickHouse does not fit as a counter store but the main purpose of
-// this repository is to explore different kind of SGBD and setup testcontainers
-// to run integration tests.
+// NOTE: ClickHouse does not fit as a counter store but the main purpose
+// of this repository is to explore how to setup testcontainers
+// to run integration tests with different kind of SGBD.
 package clickhouse
 
 import (
--- internals/redis/client.go
+++ internals/redis/client.go
@@ -1,3 +1,6 @@
+// redis package define a client to send request to a Redis server.
+// It implements repository.Repository interface in order to run
+// business logic related to our counter component.
 package redis
 
 import (
